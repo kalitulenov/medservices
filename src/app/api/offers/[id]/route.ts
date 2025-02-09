fbd1f0f558526696f6d724df6bbf712b69bccf44
unknown <kalitulenov@mail.ru>
2025-02-09 16:44:11 -0500

    093.02.2025

--- src/app/api/offers/[id]/route.ts
+++ src/app/api/offers/[id]/route.ts
@@ -23,24 +23,41 @@
 import { NextRequest, NextResponse } from 'next/server';
 import { db } from '@/lib/db'; // Предположим, что db — это ваш Prisma клиент
 
-export async function DELETE(req: NextRequest, { params }: { params: { id: string } }) {
-    const id = parseInt(params.id, 10); // Преобразуем id в число
+// export async function DELETE(req: NextRequest, { params }: { params: { id: string } }) {
+//     const id = parseInt(params.id, 10); // Преобразуем id в число
 
-    if (isNaN(id)) {
-        return NextResponse.json({ error: 'Invalid ID' }, { status: 400 });
-    }
+//     if (isNaN(id)) {
+//         return NextResponse.json({ error: 'Invalid ID' }, { status: 400 });
+//     }
 
-    try {
-        await db.spruslfrm.delete({
-            where: { id: id }
-        });
-        return NextResponse.json({ id: id });
-    } catch (err) {
-        console.error(err);
-        return NextResponse.json({ error: 'Failed to delete record' }, { status: 500 });
-    }
+//     try {
+//         await db.spruslfrm.delete({
+//             where: { id: id }
+//         });
+//         return NextResponse.json({ id: id });
+//     } catch (err) {
+//         console.error(err);
+//         return NextResponse.json({ error: 'Failed to delete record' }, { status: 500 });
+//     }
+// }
+
+export async function DELETE(req: NextRequest, context: { params: { id: string } }) {
+  const id = Number(context.params.id);
+
+  if (!id) {
+      return NextResponse.json({ error: 'Invalid ID' }, { status: 400 });
+  }
+
+  try {
+      await db.spruslfrm.delete({
+          where: { id }
+      });
+      return NextResponse.json({ id });
+  } catch (err) {
+      console.error(err);
+      return NextResponse.json({ error: 'Failed to delete record' }, { status: 500 });
+  }
 }
-
 
   export async function PUT(req: Request, {params}: {params: {id: string}} ) {
     const id = params.id;
